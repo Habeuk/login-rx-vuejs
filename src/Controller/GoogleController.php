5e60a958f19ac77488630a292ba0d69d163ba73c
stephane888 <kksasteph888@gmail.com>
2021-10-21 11:43:17 +0200

    MAJ3

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -13,135 +13,129 @@ use Drupal\Component\Utility\Random;
 use Google\Client as GoogleClient;
 use Google\Service\Oauth2;
 use Drupal\login_rx_vuejs\SdkGoogle;
+use Drupal\login_rx_vuejs\Services\loginRxVuejsException;
 
-
-class GoogleController extends ControllerBase
-{
-
-	protected $UserAuth;
-
-	public function __construct(UserAuth $UserAuth)
-	{
-		$this->UserAuth = $UserAuth;
-	}
-
-	/**
-	 *
-	 * {@inheritdoc}
-	 */
-	public static function create(ContainerInterface $container)
-	{
-		return new static($container->get('login_rx_vuejs.identification'));
-	}
-
-	/**
-	 * Essaie de connecter l'utilisteur ou cree un compte à partir de son access_token facebook'.
-	 */
-	public function CheckUser(Request $Request)
-	{
-		$content = $Request->getContent();
-		$googleUser = Json::decode($content);
-		$content = SdkGoogle::loadCurrentUserInfo($googleUser);
-		if(! empty($content['email'])) {
-			$uid = $this->UserAuth->loadByMailOrLogin($content['email']);
-			if($uid)
-				return $this->reponse($this->UserAuth->connectUser($uid));
-			else
-				return $this->reponse([
-						'createuser' => true
-				]);
-		}
-		return $this->reponse($content);
-	}
-
-	/**
-	 * Essaie de connecter l'utilisteur ou cree un compte à partir de son access_token facebook'.
-	 */
-	public function Connexion(Request $Request)
-	{
-		$datas = $Request->getContent();
-		$datas = Json::decode($datas);
-		$content = $datas['google'];
-		$user = $datas['fields'];
-
-		if(! empty($content['name']) && ! empty($content['email'])) {
-			$uid = $this->UserAuth->loadByMailOrLogin($content['email']);
-			if($uid)
-				return $this->reponse($this->UserAuth->connectUser($uid));
-			else {
-				// On cree le compte.
-				$Random = new Random();
-				$username = Html::getId($content['name']) . "--" . time();
-				$user['name'] = [
-						'value' => $username
-				];
-				$user['pass'] = [
-						'value' => $Random->string(8, true)
-				];
-				$user['mail'] = [
-						'value' => $content['email']
-				];
-				$uid = $this->UserAuth->createUser($user);
-				return $this->reponse($this->UserAuth->connectUser($uid));
-			}
-		}
-		return $this->reponse($content);
-	}
-
-	/**
-	 *
-	 * @return \Symfony\Component\HttpFoundation\JsonResponse
-	 */
-	public function Callback()
-	{
-		$clientID = $cf_g['app_id'];
-		$clientSecret = $cf_g['app_secret'];
-		$redirectUri = $cf_g['redirectUri'];
-		$client = new GoogleClient();
-		$client->setClientId($clientID);
-		$client->setClientSecret($clientSecret);
-		$client->setRedirectUri($redirectUri);
-		$client->addScope([
-				"email",
-				"profile"
-		]);
-		if(isset($_GET['code'])) {
-			$token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-
-			$client->setAccessToken($token['access_token']);
-			// get profile info
-			$google_oauth = new Oauth2($client);
-			$google_account_info = $google_oauth->userinfo->get();
-			$results = [
-					'id' => $google_account_info->id,
-					'first_name' => $google_account_info->givenName,
-					'last_name' => $google_account_info->familyName,
-					'name' => $google_account_info->givenName . ' ' . $google_account_info->familyName,
-					'email' => $google_account_info->email,
-					'picture' => $google_account_info->picture
-			];
-		}
-		$content = [ ];
-		return $this->reponse($content);
-	}
-
-	/**
-	 *
-	 * @param array|string $configs
-	 * @param number $code
-	 * @param string $message
-	 * @return \Symfony\Component\HttpFoundation\JsonResponse
-	 */
-	protected function reponse($configs, $code = null, $message = null)
-	{
-		if(! is_string($configs))
-			$configs = Json::encode($configs);
-		$reponse = new JsonResponse();
-		if($code)
-			$reponse->setStatusCode($code, $message);
-		$reponse->setContent($configs);
-		return $reponse;
-	}
-
-
+class GoogleController extends ControllerBase {
+  protected $UserAuth;
+  
+  public function __construct(UserAuth $UserAuth) {
+    $this->UserAuth = $UserAuth;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('login_rx_vuejs.identification'));
+  }
+  
+  /**
+   * Essaie de connecter l'utilisteur ou cree un compte à partir de son
+   * access_token facebook'.
+   */
+  public function CheckUser(Request $Request) {
+    try {
+      $content = $Request->getContent();
+      $googleUser = Json::decode($content);
+      $content = SdkGoogle::loadCurrentUserInfo($googleUser);
+      if (!empty($content['email'])) {
+        $uid = $this->UserAuth->loadByMailOrLogin($content['email']);
+        if ($uid)
+          return $this->reponse($this->UserAuth->connectUser($uid));
+        else
+          return $this->reponse(['createuser' => true
+          ]);
+      }
+      return $this->reponse($content);
+    }
+    catch (loginRxVuejsException $e) {
+      return $this->reponse($e->getTrace(), 401, $e->getMessage());
+    }
+    catch (\Exception $e) {
+      return $this->reponse($e->getTrace(), 401, $e->getMessage());
+    }
+  }
+  
+  /**
+   * Essaie de connecter l'utilisteur ou cree un compte à partir de son
+   * access_token facebook'.
+   */
+  public function Connexion(Request $Request) {
+    $datas = $Request->getContent();
+    $datas = Json::decode($datas);
+    $content = $datas['google'];
+    $user = $datas['fields'];
+    
+    if (!empty($content['name']) && !empty($content['email'])) {
+      $uid = $this->UserAuth->loadByMailOrLogin($content['email']);
+      if ($uid)
+        return $this->reponse($this->UserAuth->connectUser($uid));
+      else {
+        // On cree le compte.
+        $Random = new Random();
+        $username = Html::getId($content['name']) . "--" . time();
+        $user['name'] = ['value' => $username
+        ];
+        $user['pass'] = ['value' => $Random->string(8, true)
+        ];
+        $user['mail'] = ['value' => $content['email']
+        ];
+        $uid = $this->UserAuth->createUser($user);
+        return $this->reponse($this->UserAuth->connectUser($uid));
+      }
+    }
+    return $this->reponse($content);
+  }
+  
+  /**
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+  public function Callback() {
+    $clientID = $cf_g['app_id'];
+    $clientSecret = $cf_g['app_secret'];
+    $redirectUri = $cf_g['redirectUri'];
+    $client = new GoogleClient();
+    $client->setClientId($clientID);
+    $client->setClientSecret($clientSecret);
+    $client->setRedirectUri($redirectUri);
+    $client->addScope(["email", "profile"
+    ]);
+    if (isset($_GET['code'])) {
+      $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+      
+      $client->setAccessToken($token['access_token']);
+      // get profile info
+      $google_oauth = new Oauth2($client);
+      $google_account_info = $google_oauth->userinfo->get();
+      $results = ['id' => $google_account_info->id,
+        'first_name' => $google_account_info->givenName,
+        'last_name' => $google_account_info->familyName,
+        'name' => $google_account_info->givenName . ' ' .
+        $google_account_info->familyName, 'email' => $google_account_info->email,
+        'picture' => $google_account_info->picture
+      ];
+    }
+    $content = [];
+    return $this->reponse($content);
+  }
+  
+  /**
+   *
+   * @param array|string $configs
+   * @param number $code
+   * @param string $message
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+  protected function reponse($configs, $code = null, $message = null) {
+    if (!is_string($configs))
+      $configs = Json::encode($configs);
+    $reponse = new JsonResponse();
+    if ($code)
+      $reponse->setStatusCode($code, $message);
+    $reponse->setContent($configs);
+    return $reponse;
+  }
+  
 }
--- src/SdkGoogle.php
+++ src/SdkGoogle.php
@@ -4,44 +4,39 @@ namespace Drupal\login_rx_vuejs;
 
 use Google\Client as GoogleClient;
 use Google\Service\Oauth2;
+use Drupal\login_rx_vuejs\Services\loginRxVuejsException;
 
-
-class SdkGoogle
-{
-
-	static function loadCurrentUserInfo($googleUser)
-	{
-		// access_token
-		if(! empty($googleUser['client_id'])) {
-			$client = new GoogleClient([
-					'client_id' => $googleUser['client_id']
-			]); // Specify the CLIENT_ID of the app that accesses the backend
-			$payload = $client->verifyIdToken($googleUser['id_token']);
-			if($payload) {
-				return $payload;
-			}
-			else {
-				throw new \Exception("Token id invalide");
-			}
-		}
-		elseif(! empty($googleUser['access_token'])) {
-			$client = new GoogleClient();
-			$client->setAccessToken($googleUser['access_token']);
-			$google_oauth = new Oauth2($client);
-			$google_account_info = $google_oauth->userinfo->get();
-			return [
-					'id' => $google_account_info->id,
-					'first_name' => $google_account_info->givenName,
-					'last_name' => $google_account_info->familyName,
-					'name' => $google_account_info->givenName . ' ' . $google_account_info->familyName,
-					'email' => $google_account_info->email,
-					'picture' => $google_account_info->picture
-			];
-		}
-		else {
-			throw new \Exception("identifiant non definit");
-		}
-	}
-
-
+class SdkGoogle {
+  
+  static function loadCurrentUserInfo($googleUser) {
+    // access_token
+    if (!empty($googleUser['client_id'])) {
+      $client = new GoogleClient(['client_id' => $googleUser['client_id']
+      ]); // Specify the CLIENT_ID of the app that accesses the backend
+      $payload = $client->verifyIdToken($googleUser['id_token']);
+      if ($payload) {
+        return $payload;
+      }
+      else {
+        throw new loginRxVuejsException("Token id invalide");
+      }
+    }
+    elseif (!empty($googleUser['access_token'])) {
+      $client = new GoogleClient();
+      $client->setAccessToken($googleUser['access_token']);
+      $google_oauth = new Oauth2($client);
+      $google_account_info = $google_oauth->userinfo->get();
+      return ['id' => $google_account_info->id,
+        'first_name' => $google_account_info->givenName,
+        'last_name' => $google_account_info->familyName,
+        'name' => $google_account_info->givenName . ' ' .
+        $google_account_info->familyName, 'email' => $google_account_info->email,
+        'picture' => $google_account_info->picture
+      ];
+    }
+    else {
+      throw new loginRxVuejsException("identifiant non definit");
+    }
+  }
+  
 }
