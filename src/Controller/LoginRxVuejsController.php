b50bbf036d7a30711826848adb6f7365d44ee937
stephane888 <kksasteph888@gmail.com>
2022-05-19 15:55:55 +0200

    MAJ

--- src/Controller/LoginRxVuejsController.php
+++ src/Controller/LoginRxVuejsController.php
@@ -11,102 +11,104 @@ use Drupal\login_rx_vuejs\Services\loginRxVuejs as UserAuth;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\login_rx_vuejs\Services\loginRxVuejsException;
 
-
 /**
  * Returns responses for Login rx vuejs routes.
  */
-class LoginRxVuejsController extends ControllerBase
-{
-
-	protected $UserAuth;
-
-	public function __construct(UserAuth $UserAuth)
-	{
-		$this->UserAuth = $UserAuth;
-	}
-
-	/**
-	 *
-	 * {@inheritdoc}
-	 */
-	public static function create(ContainerInterface $container)
-	{
-		// return new static($container->get('prestashop_rest_api.cron'), $container->get('prestashop_rest_api.build_product_to_drupal'));
-		return new static($container->get('login_rx_vuejs.identification'));
-	}
-
-	/**
-	 * Builds the response.
-	 */
-	public function build()
-	{
-		$build['content'] = [
-				'#type' => 'item',
-				'#markup' => $this->t('It works!')
-		];
-		return $build;
-	}
-
-	/**
-	 * Connexion de l'utilisateur
-	 */
-	public function userConnexion(Request $Request)
-	{
-		$code = 200;
-		$msg = "";
-		$content = $Request->getContent();
-		$content = Json::decode($content);
-		$password = ! empty($content['password']) ? $content['password'][0]['value'] : null;
-		$login = ! empty($content['name']) ? $content['name'][0]['value'] : null;
-		try {
-			$content = $this->UserAuth->authentification($login, $password);
-		} //
-		catch ( loginRxVuejsException $e ) {
-			$msg = $e->getMessage();
-			$code = $e->getCode();
-			$content["loginRxVuejsException"] = $e->getTrace();
-		} //
-		catch ( \Exception $e ) {
-			$msg = $e->getMessage();
-			$code = $e->getCode();
-			$content["Exception"] = $e->getTrace();
-		} //
-		catch ( \Error $e ) {
-			$msg = $e->getMessage();
-			$code = $e->getCode();
-			$content["Error"] = $e->getTrace();
-		}
-		return $this->reponse($content, $code, $msg);
-	}
-
-	public function CheckUserStatus(Request $Request)
-	{
-		$content = Json::decode($Request->getContent());
-		$login = ! empty($content['name']) ? $content['name'][0]['value'] : null;
-		$content = null;
-		$msg = "";
-		$code = 200;
-		if ($login) {
-			$content = $this->UserAuth->loadByMailOrLogin($login);
-		}
-		return $this->reponse($content, $code, $msg);
-	}
-
-	/**
-	 *
-	 * @param array|string $configs
-	 * @param number $code
-	 * @param string $message
-	 * @return \Symfony\Component\HttpFoundation\JsonResponse
-	 */
-	protected function reponse($configs, $code = null, $message = null)
-	{
-		if (! is_string($configs))
-			$configs = Json::encode($configs);
-		$reponse = new JsonResponse();
-		if ($code)
-			$reponse->setStatusCode($code, $message);
-		$reponse->setContent($configs);
-		return $reponse;
-	}
+class LoginRxVuejsController extends ControllerBase {
+  protected $UserAuth;
+  
+  public function __construct(UserAuth $UserAuth) {
+    $this->UserAuth = $UserAuth;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // return new static($container->get('prestashop_rest_api.cron'),
+    // $container->get('prestashop_rest_api.build_product_to_drupal'));
+    return new static($container->get('login_rx_vuejs.identification'));
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\Core\Controller\ControllerBase::currentUser()
+   */
+  public function currentUser() {
+    $id = \Drupal::currentUser()->id();
+    if ($id) {
+      $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+      $serializer = \Drupal::service('serializer');
+      $data = $serializer->serialize($user, 'json', [
+        'plugin_id' => 'entity'
+      ]);
+    }
+    else {
+      $data = $id;
+    }
+    return $this->reponse($data);
+  }
+  
+  /**
+   * Connexion de l'utilisateur
+   */
+  public function userConnexion(Request $Request) {
+    $code = 200;
+    $msg = "";
+    $content = $Request->getContent();
+    $content = Json::decode($content);
+    $password = !empty($content['password']) ? $content['password'][0]['value'] : null;
+    $login = !empty($content['name']) ? $content['name'][0]['value'] : null;
+    try {
+      $content = $this->UserAuth->authentification($login, $password);
+    } //
+    catch (loginRxVuejsException $e) {
+      $msg = $e->getMessage();
+      $code = $e->getCode();
+      $content["loginRxVuejsException"] = $e->getTrace();
+    } //
+    catch (\Exception $e) {
+      $msg = $e->getMessage();
+      $code = $e->getCode();
+      $content["Exception"] = $e->getTrace();
+    } //
+    catch (\Error $e) {
+      $msg = $e->getMessage();
+      $code = $e->getCode();
+      $content["Error"] = $e->getTrace();
+    }
+    return $this->reponse($content, $code, $msg);
+  }
+  
+  public function CheckUserStatus(Request $Request) {
+    $content = Json::decode($Request->getContent());
+    $login = !empty($content['name']) ? $content['name'][0]['value'] : null;
+    $content = null;
+    $msg = "";
+    $code = 200;
+    if ($login) {
+      $content = $this->UserAuth->loadByMailOrLogin($login);
+    }
+    return $this->reponse($content, $code, $msg);
+  }
+  
+  /**
+   *
+   * @param array|string $configs
+   * @param number $code
+   * @param string $message
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+  protected function reponse($configs, $code = null, $message = null) {
+    if (!is_string($configs))
+      $configs = Json::encode($configs);
+    $reponse = new JsonResponse();
+    if ($code)
+      $reponse->setStatusCode($code, $message);
+    $reponse->setContent($configs);
+    return $reponse;
+  }
+  
 }
