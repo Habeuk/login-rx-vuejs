6a087ec42dcbff6043de64b3c0c04d49b597dc02
kouwa stephane <kksasteph888@gmail.com>
2023-08-07 09:15:57 +0200

    Maj

---
+++ README.md
@@ -0,0 +1,16 @@
+# Drupal module : login_rx_vuejs
+
+Module de connexion en js.
+
+## installation
+
+```
+composer require habeuk/login_rx_vuejs
+```
+
+### Author
+
+<div>
+<img alt="Logo habeuk" src="https://habeuk.com/sites/default/files/styles/medium/public/2023-08/logo-habeuk.png" height="40px">
+<strong> Provide by <a href="https://habeuk.com/" target="_blank"> habeuk.com </a> </strong>
+</div>
--- src/SdkGoogle.php
+++ src/SdkGoogle.php
@@ -8,10 +8,15 @@ use Drupal\login_rx_vuejs\Services\loginRxVuejsException;
 
 class SdkGoogle {
   
+  static function testAuth() {
+    $client = new GoogleClient();
+  }
+  
   static function loadCurrentUserInfo($googleUser) {
     // access_token
     if (!empty($googleUser['client_id'])) {
-      $client = new GoogleClient(['client_id' => $googleUser['client_id']
+      $client = new GoogleClient([
+        'client_id' => $googleUser['client_id']
       ]);
       $payload = null;
       // Specify the CLIENT_ID of the app that accesses the backend
@@ -32,11 +37,12 @@ class SdkGoogle {
       $client->setAccessToken($googleUser['access_token']);
       $google_oauth = new Oauth2($client);
       $google_account_info = $google_oauth->userinfo->get();
-      return ['id' => $google_account_info->id,
+      return [
+        'id' => $google_account_info->id,
         'first_name' => $google_account_info->givenName,
         'last_name' => $google_account_info->familyName,
-        'name' => $google_account_info->givenName . ' ' .
-        $google_account_info->familyName, 'email' => $google_account_info->email,
+        'name' => $google_account_info->givenName . ' ' . $google_account_info->familyName,
+        'email' => $google_account_info->email,
         'picture' => $google_account_info->picture
       ];
     }
